bfe16c28aab61ebcf6226833eb5ae51f07c0ab39
Andrey Utkin <andrey.utkin@corp.bluecherry.net>
2014-08-12 06:33:31 -0500

    chmod a+x first_boot.sh

--- preseed.sh
+++ preseed.sh
@@ -2,6 +2,7 @@
 
 wget http://distro.bluecherrydvr.com/ubuntu/installer/first_boot.sh -O /target/root/first_boot.sh
 
+chmod a+x /target/root/first_boot.sh
 cp /target/etc/rc.local{,.bkp}
 
 cat << 'EOF' > /target/etc/rc.local
