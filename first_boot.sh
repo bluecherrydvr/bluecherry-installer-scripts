f37f34be8072b2c3a7120f87415f4adae0c04665
Andrey Utkin <andrey.krieger.utkin@gmail.com>
2015-11-03 03:03:39 +0200

    Update first_boot.sh

--- first_boot.sh
+++ first_boot.sh
@@ -25,6 +25,9 @@ apt-get update
 apt-get remove --yes \
 	ubuntu-artwork \
 
+apt-get install --yes --verbose-versions \
+	linux-image-generic \
+	linux-headers-generic \
 
 apt-get install --yes --verbose-versions \
 	mysql-server \
@@ -33,30 +36,14 @@ apt-get install --yes --verbose-versions \
 	bluecherry-artwork \
 	plymouth-theme-bluecherry-logo \
 	gnome-session-flashback \
+	bluecherry \
+	bluecherry-client \
 
-#	bluecherry \
-
-
-# TODO Populate package with fixed postinst into repos and install via apt-get
-if [[ `arch` == 'x86_64' ]]
-then
-	ARCH='amd64'
-else
-	ARCH='i386'
-fi
-
-BC_VER=2.3.14
-wget "http://lizard.bluecherry.net/~autkin/release_${BC_VER}/trusty/bluecherry_${BC_VER}_${ARCH}.deb" -O /root/bc.deb
-dpkg -i /root/bc.deb || true
-
-wget "http://downloads.bluecherrydvr.com/client/2.2.0/bluecherry-client_2.2.0-1_${ARCH}.deb" -O /root/bc-client.deb
-dpkg -i /root/bc-client.deb || true
 
 apt-get --yes -f install
 
 mv /etc/rc.local{.bkp,}
 rm $0
-rm /root/bc.deb
 
 echo -e "[SeatDefaults]\nuser-session=gnome-fallback" >> /usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf
 
